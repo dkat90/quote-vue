814501f3cd898cf824cf7a596bce99f1b44b33ad
DKAT <dkat90@gmail.com>
2016-12-23 11:13:30 +0800

    initial and final commit

---
+++ src/App.vue
@@ -0,0 +1,50 @@
+<template>
+    <div class="container">
+      <appHeader :quoteCount="quotes.length" :maxQuotes="maxQuotes"></appHeader>
+      <appNewQuote @quoteAdded="newQuote"></appNewQuote>
+      <appQuoteGrid :quotes="quotes" @quoteDeleted="deleteQuote"></appQuoteGrid>
+      <div class="row">
+        <div class="col-sm-12 text-center">
+          <div class="alert alert-info">
+            Info: Click on a quote to delete it!
+          </div>
+        </div>
+      </div>
+    </div>
+</template>
+
+<script>
+    import QuoteGrid from "./components/QuoteGrid.vue";
+    import NewQuote from "./components/NewQuote.vue";
+    import Header from "./components/Header.vue";
+
+    export default {
+        data: function(){
+          return {
+            maxQuotes: 10,
+            quotes: [
+              'Just a Normal Quote',
+            ]
+          }
+        },
+        components: {
+          appQuoteGrid: QuoteGrid,
+          appNewQuote: NewQuote,
+          appHeader: Header
+        },
+        methods: {
+          newQuote(quote) {
+            if(this.quotes.length >= this.maxQuotes ){
+              return alert('Please Delete a Quote')
+            }
+            this.quotes.push(quote);
+          },
+          deleteQuote(index){
+            this.quotes.splice(index, 1);
+          }
+        }
+    }
+</script>
+
+<style>
+</style>
---
+++ src/components/QuoteGrid.vue
@@ -0,0 +1,24 @@
+<template id="">
+  <div class="row">
+    <appQuote v-for="(quote, index) in quotes" @click.native="deleteQuote(index)">{{ quote }}</appQuote>
+  </div>
+</template>
+
+<script type="text/javascript">
+  import Quote from "./Quote.vue"
+  export default {
+    props: ['quotes'],
+    components:{
+      appQuote: Quote
+    },
+    methods: {
+      deleteQuote(index){
+        this.$emit('quoteDeleted', index);
+      }
+    }
+  }
+</script>
+
+<style media="screen">
+
+</style>
